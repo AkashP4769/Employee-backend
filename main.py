76fc01b970d53b047f337d84ebfe0923a8040c05
Akash <awesomeakash47@gmail.com>
2026-05-28 12:40:30 +0530

    refactor: added .env to .gitignore file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 from middleware import configure_middleware
 from database.connection import create_tables
 from routers.employee_router import router as employee_router
+from config import APP_ENV
 
 
 logging.basicConfig(
@@ -29,6 +30,10 @@ app = FastAPI(
 
 configure_middleware(app)
 app.include_router(employee_router)
+
+@app.get("/health", tags=["health"])
+def health():
+    return {"message": f"App is healthy. Environment: {APP_ENV}"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000, reload=True)
