5cd2137351ca291310bcce919af001de0968d621
Akash <awesomeakash47@gmail.com>
2026-06-01 13:52:32 +0530

    refactor: change user to employee in employee router

--- employees/router.py
+++ employees/router.py
@@ -25,25 +25,25 @@ async def get_all_employees(db: AsyncSession = Depends(get_db), _current_user: T
     return [emp for emp in employees]
 
 
-@router.get("/{user_id}", response_model=GetEmployeeResponse)
-async def get_employee(user_id: int, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)):
-    employee: Employee = await service.get_employee(db, user_id=user_id)
+@router.get("/{employee_id}", response_model=GetEmployeeResponse)
+async def get_employee(employee_id: int, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)):
+    employee: Employee = await service.get_employee(db, employee_id=employee_id)
 
     return employee
 
 
-@router.patch("/{user_id}", status_code=status.HTTP_200_OK, response_model=EmployeeResponse)
-async def patch_employee(user_id: int, body: EmployeePatch, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)):
-    id = user_id
+@router.patch("/{employee_id}", status_code=status.HTTP_200_OK, response_model=EmployeeResponse)
+async def patch_employee(employee_id: int, body: EmployeePatch, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)):
+    id = employee_id
 
     patched_employee: Employee = await service.patch_employee(db, id=id, body=body)
 
     return patched_employee
 
 
-@router.delete("/{user_id}", response_model=EmployeeResponse)
-async def delete_employee(user_id: int, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)):
-    deleted_employee: Employee = await service.delete_employee(db, user_id=user_id)
+@router.delete("/{employee_id}", response_model=EmployeeResponse)
+async def delete_employee(employee_id: int, db: AsyncSession = Depends(get_db), _current_user: TokenPayload = Depends(get_current_user)):
+    deleted_employee: Employee = await service.delete_employee(db, employee_id=employee_id)
 
     return deleted_employee
 
