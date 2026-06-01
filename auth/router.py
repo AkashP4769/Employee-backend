41f193fa750e012187613fd1637b6d159f171b03
Akash <awesomeakash47@gmail.com>
2026-06-01 13:51:45 +0530

    feat: add form with auth

--- auth/router.py
+++ auth/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -12,8 +13,8 @@ from database import get_db
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 @router.post("/login", response_model=TokenResponse)
-async def login(body: LoginRequest, db: AsyncSession = Depends(get_db)):
-    access_token, refresh_token = await auth_service.login(db, body.email, body.password)
+async def login(form: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    access_token, refresh_token = await auth_service.login(db, form.username, form.password)
 
     return TokenResponse(access_token=access_token, refresh_token=refresh_token)
 
